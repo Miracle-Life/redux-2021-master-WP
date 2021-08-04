36fe075f84776b8f5fd6bbf4a8255dfb5e76581c
Denys Bielov <belovdenys@gmail.com>
2021-08-05 00:30:20 +0300

    app in redux v1.0

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 import './styles.css'
-
-let state = 0
+import {rootReducer} from "./redux/rootReducer";
+import { createStore } from 'redux'
+import {decrement, increment} from "./redux/actions";
+// const {createStore} = require("./createStore");
+// import {DECREMENT, INCREMENT} from "./redux/types";
 const subBtn = document.getElementById('sub')
 const addBtn = document.getElementById('add')
 const counter = document.getElementById('counter')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
-function render() {
-    counter.textContent = state.toString()
-}
+const store = createStore(rootReducer, 0)
+
 
 addBtn.addEventListener('click', () => {
-    state++
-    render()
+    store.dispatch(increment())
+
 })
 subBtn.addEventListener('click', () => {
-    state--
-    render()
+    store.dispatch(decrement())
+
 })
 asyncBtn.addEventListener('click', () => {
-    setTimeout(() => {
-        state++
-        render()
-    }, 2000)
+
 })
 themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark')
+    // document.body.classList.toggle('dark')
 })
 
-
-render()
+store.subscribe(() => {
+    // console.log(store.getState())
+    const state = store.getState()
+    counter.textContent = state
+})
+store.dispatch({type: 'INIT__APP'})
---
+++ src/native.js
@@ -0,0 +1,35 @@
+import './styles.css'
+
+let state = 0
+const subBtn = document.getElementById('sub')
+const addBtn = document.getElementById('add')
+const counter = document.getElementById('counter')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
+
+function render() {
+    counter.textContent = state.toString()
+}
+
+addBtn.addEventListener('click', () => {
+    state++
+    render()
+})
+subBtn.addEventListener('click', () => {
+    state--
+    render()
+})
+asyncBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        state++
+        render()
+    }, 2000)
+})
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
+
+
+render()
+
+
